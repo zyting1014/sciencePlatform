e764ab036a0940e4c0357420c3d43da6e241e259
zyting1014 <16301083@bjtu.edu.cn>
2020-02-08 14:38:21 +0800

    add progressBar

--- sciencePlatform/cpoModel/urls.py
+++ sciencePlatform/cpoModel/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('index/', views.index, name='index'), # 首页
     path('comment/',views.comment,name='comment'), # 用户满意度数据
+    path('mainModel/',views.mainModel,name='mainModel'), # 主模型配置
     path('empty/',views.empty,name='empty'), # 空网页
+    path('ajaxGetModel/',views.ajaxGetModel,name='ajaxGetModel') # ajax get方法 获取模型标题表格
  ]
--- sciencePlatform/cpoModel/views.py
+++ sciencePlatform/cpoModel/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+import json
 
 def index(request):
     return render(request,'index.html')
@@ -6,6 +8,22 @@ def index(request):
 def comment(request):
     return render(request,'comment.html')
 
+def mainModel(request):
+    return render(request,'mainModel.html')
+
 def empty(request):
     return render(request,'empty.html')
 
+
+
+def ajaxGetModel(request):
+    # return 训练集日期 训练集城市 测试集日期 测试集城市 模型名称 特征数量 正负样本比例 上传时间
+    item1 = {'trainDate': '20191001-20191007', 'trainCity': 'Beijing', 'testDate': '20191110-20191110', 'testCity': 'Beijing',
+             'modelName': 'LR', 'featureNums': '17', 'posWeight': '15', 'createTime': '20200101'}
+    item2 = {'trainDate': '20191001-20191007', 'trainCity': 'Beijing', 'testDate': '20191110-20191110', 'testCity': 'Beijing',
+             'modelName': 'LR', 'featureNums': '17', 'posWeight': '15', 'createTime': '20200101'}
+
+    data = {'data': [item1, item2]}
+
+    return HttpResponse(json.dumps(data,ensure_ascii=False), content_type='application/json')
+    # return render(request,'empty.html')
